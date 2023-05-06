35fae5a3d9c5658e6193930acca2296ed986cf8f
mosidrum <mosiokanga@gmail.com>
2023-05-06 09:25:20 +0100

    Project completed

--- module/removing.js
+++ module/removing.js
@@ -1,6 +1,7 @@
+import createTask from './display.js';
 import { updateIds } from './update.js';
 
-const removeTask = (taskId, tasks) => {
+export const removeTask = (taskId, tasks) => {
   // eslint-disable-next-line
   tasks = tasks.filter((task) => task.id !== parseInt(taskId));
   document.getElementById(taskId).remove();
@@ -8,4 +9,9 @@ const removeTask = (taskId, tasks) => {
   updateIds(tasks);
 };
 
-export default removeTask;
+export const removeCompletedTask = (tasks) => {
+  const completeTasks = tasks.filter((task )=> !task.isCompleted);
+  localStorage.setItem('tasks', JSON.stringify(completeTasks));
+  updateIds(completeTasks);
+  window.location.reload();
+}
